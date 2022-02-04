e863b552dc4f481688964d87cdb4f88aad02b718
Paulo451 <pauloszuparits@hotmail.com>
2022-02-03 21:24:59 -0300

    was added a description feature to Tarefas and was added some styles for the same page

--- my-app/src/pages/Tarefas/index.js
+++ my-app/src/pages/Tarefas/index.js
@@ -4,17 +4,22 @@ import Draggable from "react-draggable";
 export default function Tarefas(){
 
     const[tarefa, setTarefa] = useState('');
+    const[descricao, setDescricao] = useState('');
     const[tarefas, setTarefas] = useState([]);
     const[contador, setContador] = useState(0);
-
+    
     
 
     function handleAdd(){
+        
         let objTarefa = {id: contador,
                         tarefa: tarefa,
+                        descricao: descricao.length === 0 ? 'Tarefa sem descrição' : descricao, 
+                        //nessa aqui eu mandei bem
                         posicao: {x: -3, y: 10}}
         setTarefas([...tarefas, objTarefa]);
         setTarefa('');
+        setDescricao('');
         let cont = contador + 1;
         setContador(cont);
 
@@ -29,7 +34,12 @@ export default function Tarefas(){
         <div>
             <div className="titulo-tarefas-conteiner">
                 <h1 className="item-titulo-tarefa">Suas Tarefas</h1>
-                <input className="item-titulo-tarefa" type="text" value = {tarefa} onChange={(e)=>{setTarefa(e.target.value)}}/>
+                <div className="input-tarefa">
+                    <p className="item-titulo-tarefa">Titulo da Tarefa</p>
+                    <input className="item-titulo-tarefa" id="titulo-tarefa" type="text" value = {tarefa} onChange={(e)=>{setTarefa(e.target.value)}}/>
+                    <p className="item-titulo-tarefa">Descrição da Tarefa</p>
+                    <textarea className="item-titulo-tarefa" value={descricao} onChange={(e)=>{setDescricao(e.target.value)}} id="descricao-tarefa"></textarea>
+                </div>
                 <button className="item-titulo-tarefa" onClick={handleAdd}>Adicionar</button>
             </div>
 
@@ -40,8 +50,11 @@ export default function Tarefas(){
                         {tarefas.map(tarefa => (
                             <Draggable defaultPosition={tarefa.posicao} key={tarefa.id}>
                                 <div className="tarefa">
-                                    <p >{tarefa.tarefa}</p>
-                                    <button  onClick={()=>{handleDelete(tarefa.id)}}>X</button>
+                                    <p>{tarefa.tarefa}</p>
+                                    <div>
+                                        <abbr title={tarefa.descricao} id="descricao-abbr">?</abbr>
+                                        <button  onClick={()=>{handleDelete(tarefa.id)}} id="botao-delete">X</button>
+                                    </div>
                                 </div>
                             </Draggable>
                         ))}
